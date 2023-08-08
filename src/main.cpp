5873721f5845de22956bba50b733ae9bf4e3158e
makian123 <hohosmarko@gmail.com>
2023-08-08 20:25:36 +0200

    added arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,62 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <cstring>
 #include "tokenizer/tokenizer.hpp"
 #include "parser/parser.hpp"
 
+enum class Flags: char{
+	OUTPUT_FILE = 1 << 0,
+	ARCHITECTURE = 1 << 1
+};
+
 int main(int argc, char **argv){
+	if(argc < 2){
+		std::cout << "Input file not specified";
+		return 1;
+	}
+	std::vector<std::string> inFilePaths;
+	std::string outFilePath;
+	char flagActive = 0;
+
+	for(int i = 1; i < argc; ++i){
+		if(flagActive){
+			if(flagActive & (char)Flags::OUTPUT_FILE){
+				outFilePath = argv[i];
+			}
+			flagActive = 0;
+			continue;
+		}
+
+		if(!std::strcmp(argv[i], "-o")){
+			flagActive |= (char)Flags::OUTPUT_FILE;
+			continue;
+		}
+
+		inFilePaths.push_back(argv[i]);
+	}
+	if(!inFilePaths.size()){
+		std::cout << "Input file not specified";
+		return 1;
+	}
+	if(!outFilePath.length()){
+		outFilePath = "a.out";
+	}
+
+	std::ifstream inFile(inFilePaths[0]);
 	Tokenizer tokenizer;
-	tokenizer.AddLine("int lol = 12 + 4; char asd = 124 * 4 + lol;");
+
+	std::string line;
+	while(std::getline(inFile, line)){
+		tokenizer.AddLine(line);
+	}
+	inFile.close();
 
 	Parser parser(tokenizer);
 	parser.Parse();
 	
-	std::ofstream output("test.txt");
+	std::ofstream output(outFilePath);
 	output << parser.GenerateCode();
 	output.close();
 	
